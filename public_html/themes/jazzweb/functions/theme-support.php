c7f10cfd373061be267930edc21923e936521912
Unknown <njxqlus@gmail.com>
2017-10-04 22:13:34 +0300

    Add theme support

--- public_html/themes/jazzweb/functions/theme-support.php
+++ public_html/themes/jazzweb/functions/theme-support.php
@@ -6,7 +6,19 @@
 add_action('after_setup_theme',
     function ()
     {
+        //Post thumbnails
         add_theme_support('post-thumbnails');
+
+        //todo post formats
+        //add_theme_support('post-formats', ['aside', 'audio', 'gallery', 'image', 'link', 'quote', 'video',]);
+
+        //Add theme support for HTML5 Semantic Markup
+        add_theme_support('html5', ['search-form', 'comment-form', 'comment-list', 'gallery', 'caption']);
+
+        //Add theme support for document Title tag
+        add_theme_support('title-tag');
+
+        //Woocommerce support
         add_theme_support('woocommerce');
     }
 );
